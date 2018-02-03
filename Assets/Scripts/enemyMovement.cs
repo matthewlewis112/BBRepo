cd27dedf30bd1bcd0c72fe8cde06c725675e179d
Sarah <sellwein@calpoly.edu>
2018-02-03 14:31:53 -0800

    Base Code

---
+++ Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+
+	public int enemySpeed;
+	public int xMoveDirection;
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (xMoveDirection, 0));
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (xMoveDirection, 0) * enemySpeed;
+		if (hit.distance < 0.7f) {
+			//flips sprite when near object
+			flip ();
+		}
+	}
+		
+	void flip ()	{
+		//flips sprite
+		if (xMoveDirection > 0) {
+			xMoveDirection = -1;
+		} else {
+			xMoveDirection = 1;
+		}
+	}
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	public int playerSpeed = 10;
+	private bool facingRight = false;
+	public int playerJumpPower = 1250;
+	private float moveX;	//moving on x-axis
+	public bool isGrounded;
+
+	// Update is called once per frame
+	void Update () {
+		playerMove ();
+	}
+
+	void playerMove()	{
+		//controls
+		moveX = Input.GetAxis("Horizontal");
+		if (Input.GetButtonDown ("Jump") && isGrounded == true)	{
+			//player can jump if jump button is pressed and if grounded
+			jump ();
+		}
+		//animation
+		//player direction
+		if (moveX < 0.0f && facingRight == false) {	
+			//flips player when moving towards a direction
+			flipPlayer ();
+		} else if (moveX > 0.0f && facingRight == true) {
+			flipPlayer ();
+		}
+		//physics
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void jump()	{
+		//jumping code
+		GetComponent<Rigidbody2D>().AddForce (Vector2.up * playerJumpPower);
+		isGrounded = false;
+	}
+
+	void flipPlayer()	{	
+		//function for flipping player
+		facingRight = !facingRight;
+		Vector2 localScale = gameObject.transform.localScale;
+		localScale.x *= -1;
+		transform.localScale = localScale;
+	}
+
+	void OnCollisionEnter2D (Collision2D col)	{
+		if (col.gameObject.tag == "ground") {
+			isGrounded = true;
+		}
+	}
+}
